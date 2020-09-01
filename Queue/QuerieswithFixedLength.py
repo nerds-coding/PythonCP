a8b7bc4568ac62193e3154269ed0b64b5e9881ed
Anup Prakash <2000.anupprakash@gmail.com>
2020-09-02 02:10:09 +0530

    Queries With Fixed Lenth in Queue

--- Queue/QuerieswithFixedLength.py
+++ Queue/QuerieswithFixedLength.py
@@ -1,1 +1,20 @@
-print(min(33, 11, 44, 11, 55))
+def solve(arr, queries):
+    result = []
+    queue = [min(queries)]
+    m = 2
+    for x in arr:
+        j = len(queries)-x
+        if(j != 0):
+            for i in range(j):
+                result.append(max(queries[i:i+m]))
+                # print(max(queries[i:i+m]))
+            m += 1
+            queue.append(min(result))
+
+        result = []
+    print(queue)
+
+
+q = [33, 11, 44, 11, 55]
+arr = [1, 2, 3, 4, 5]
+solve(arr, q)
