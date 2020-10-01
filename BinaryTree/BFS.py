d05e379d8a620b33e1e8bc86d1f69980d99add1a
Anup Prakash <2000.anupprakash@gmail.com>
2020-10-02 00:28:46 +0530

    Adding BFS

---
+++ BinaryTree/BFS.py
@@ -0,0 +1,47 @@
+class Node:
+
+    def __init__(self, value):
+        self.data = value
+        self.left = None
+        self.right = None
+
+
+def insert(root, value):
+    if(not(root))or (root.data == value):
+        return root
+    else:
+        if(root.data > value):
+            if(not(root.left)):
+                root.left = Node(value)
+            else:
+                insert(root.left, value)
+        else:
+            if(not(root.right)):
+                root.right = Node(value)
+            else:
+                insert(root.right, value)
+
+
+def inorder(root):
+    if(root):
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+
+
+root = Node(5)
+insert(root, 1)
+insert(root, 2)
+insert(root, 3)
+insert(root, 4)
+insert(root, 5)
+insert(root, 6)
+insert(root, 7)
+insert(root, 8)
+insert(root, 9)
+insert(root, 0)
+
+
+# inorder(root)
+
+bfs(root)
