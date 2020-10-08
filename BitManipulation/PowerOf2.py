1219729accee0e7877b0b3e75816a40d2da7c6e5
Anup Prakash <2000.anupprakash@gmail.com>
2020-10-08 23:23:17 +0530

    Bit Manipulation

---
+++ BitManipulation/PowerOf2.py
@@ -0,0 +1,10 @@
+def PowerOf(num):
+    print(1 << num)
+
+
+def Divide(num):
+    print(num >> 1)
+
+
+PowerOf(3)
+Divide(256)
---
+++ BitManipulation/PowerOf2OrNot.py
@@ -0,0 +1,5 @@
+def Power(num):
+    return True if(not(num & (num-1))) else False
+
+
+print(Power(4))
