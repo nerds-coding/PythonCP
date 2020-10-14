1b710804eff3ab1c5b18a0009582b102d4465fef
Anup Prakash <2000.anupprakash@gmail.com>
2020-10-15 00:16:15 +0530

    Graph

--- BinaryTree/BFS.py
+++ BinaryTree/BFS.py
@@ -1,59 +1,55 @@
 class Node:
 
-    def __init__(self, value):
-        self.data = value
-        self.left = None
-        self.right = None
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
 
 
-def insert(root, value):
-    if(not(root))or (root.data == value):
-        return root
+def insert(root, key):
+    if (root is None):
+        return Node(key)
     else:
-        if(root.data > value):
-            if(not(root.left)):
-                root.left = Node(value)
-            else:
-                insert(root.left, value)
-        else:
-            if(not(root.right)):
-                root.right = Node(value)
-            else:
-                insert(root.right, value)
+        if(root.data == key):
+            return root
+        elif(root.data > key):
+            root.left = insert(root.left, key)
+        elif(root.data < key):
+            root.right = insert(root.right, key)
+    return root
 
 
 def inorder(root):
-    if(root):
+    if root:
         inorder(root.left)
         print(root.data)
         inorder(root.right)
 
 
 def bfs(root):
-    if(not(root)):
-        return root
-    q = [root]
-    while(len(q) > 0):
-        node = q.pop(0)
-        print(node.data)
-
-        if(node.left):
-            q.append(node.left)
-        if(node.right):
-            q.append(node.right)
+    if root:
+        q = [root]
+        while q:
+            d = q.pop(0)
+            print(d.data, end=' ')
+            if (d.left):
+                q.append(d.left)
+            if (d.right):
+                q.append(d.right)
+            print()
 
 
 root = Node(5)
-insert(root, 1)
-insert(root, 2)
-insert(root, 3)
-insert(root, 4)
-insert(root, 5)
-insert(root, 6)
-insert(root, 7)
-insert(root, 8)
-insert(root, 9)
-insert(root, 0)
+root = insert(root, 1)
+root = insert(root, 2)
+root = insert(root, 3)
+root = insert(root, 4)
+root = insert(root, 5)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 8)
+root = insert(root, 9)
+root = insert(root, 0)
 
 
 # inorder(root)
