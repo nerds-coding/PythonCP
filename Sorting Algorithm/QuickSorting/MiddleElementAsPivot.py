0d80d97d03e73d1be870a640141b88deed6d700d
Anup Prakash <2000.anupprakash@gmail.com>
2020-11-05 12:30:42 +0530

    Adding QuickSort Pivot as middle element

--- Sorting Algorithm/QuickSorting/MiddleElementAsPivot.py
+++ Sorting Algorithm/QuickSorting/MiddleElementAsPivot.py
@@ -1,34 +1,26 @@
-def partition(arr,l,h):
+def quickSort(arr, l, h):
     i = l
     j = h
-    half = len(arr)//2
-    pivot = arr[half]
+    mid = (l+h)//2
+    pivot = arr[mid]
 
-    while(i<j):
-        while(arr[i]<pivot)and(i<h):
-            i+=1
-        while(arr[j]>pivot)and(j>l):
-            j-=1
-        if(i<j):
-            arr[i],arr[j] = arr[j],arr[i]
-            i+=1
-            j-=1
-        elif(j==half)or(i==half):
-            break
-        else:
-            i+=1
-    return half
+    while(i < j):
+        while(arr[i] < pivot):
+            i += 1
+        while(arr[j] > pivot):
+            j -= 1
+        if(i < j):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
 
-def quickSort(arr,l,h):
-    if(l<h):
-        q = partition(arr,l,h)
-        quickSort(arr,l,q-1)
-        quickSort(arr,q+1,h)
+    if(l < j):
+        quickSort(arr, l, mid-1)
+    if(i < h):
+        quickSort(arr, mid+1, h)
 
 
-arr = [34, 56, 2, 1, 45, 67, 89, 90, 323, 56, 787, 8900]
+arr = [34, 2, 1, 45, 67, 89, 90, 323, 56, 787, 8900]
 quickSort(arr, 0, len(arr)-1)
 
 print(arr)
-    
-    
