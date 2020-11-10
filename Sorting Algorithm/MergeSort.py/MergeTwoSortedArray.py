90f01801ee7d7049e277ac0f8ee057bce9a33302
Anup Prakash <2000.anupprakash@gmail.com>
2020-11-10 09:50:03 +0530

    Adding Merging Two Sorted Array

--- Sorting Algorithm/MergeSort.py/MergeTwoSortedArray.py
+++ Sorting Algorithm/MergeSort.py/MergeTwoSortedArray.py
@@ -1,29 +1,28 @@
-def Merge(arr1, arr2):
-    if(arr1 is None)and(arr2 is None):
-        return
+def Merge(arr1,arr2):
+    l1 = len(arr1)
+    l2 = len(arr2)
 
-    sorted = []
-    len1 = len(arr1)
-    len2 = len(arr2)
+    sorted_arr = []
+    i=j=k=0
 
-    i = 0
-    j = 0
-
-    while (i < len1)and(j < len2):
-        if(arr1[i] <= arr2[j]):
-            sorted.append(arr1[i])
-            i += 1
+    while(i<l1)and(j<l2):
+        if(arr1[i]<=arr2[j]):
+            sorted_arr.append(arr1[i])
+            i+=1
         else:
-            sorted.append(arr2[j])
-            j += 1
-
-    while (arr1 != []) and(i != len1):
-        sorted.append(arr1.pop(0))
-    while(arr2 != []) and(j != len2):
-        sorted.append(arr2.pop(0))
-
-    return sorted
-
+            sorted_arr.append(arr2[j])
+            j+=1
+        
+    
+    while(i<l1):
+        sorted_arr.append(arr1[i])
+        i+=1
+    
+    while(j<l2):
+        sorted_arr.append(arr2[j])
+        j+=1
+    
+    return sorted_arr
 
 arr1 = [1, 2, 3, 4, 6]
 arr2 = [6, 7, 9]
--- Sorting Algorithm/QuickSorting/MiddleElementAsPivot.py
+++ Sorting Algorithm/QuickSorting/MiddleElementAsPivot.py
@@ -1,19 +1,23 @@
-def partition(arr,l,h):
-    i = l
-    mid = (l+h)//2
-    pivot = arr[mid]
-
-    for j in range(l,h+1):
-        if(arr[j]<pivot):
-            arr[j],arr[i] = arr[i],arr[j]
-            i += 1
-    return mid
-
 def quickSort(arr,l,h):
-    if(l<h):
-        q= partition(arr,l,h)
-        quickSort(arr,l,q-1)
-        quickSort(arr,q+1,h)
+    i = l
+    j = h
+    pivot = arr[(l+h)//2]
+
+    while(i<=j):
+        while(arr[i]<pivot):
+            i+=1
+        while(arr[j]>pivot):
+            j-=1
+        if(i<=j):
+            arr[i],arr[j] = arr[j],arr[i]
+            i+=1
+            j-=1
+
+    if(l<j):
+        quickSort(arr,l,j)
+    if(i<h):
+        quickSort(arr,i,h)
+
 
 arr = [34, 2, 1, 45, 67, 667,7899,89, 90, 323, 56, 787, 8900]
 quickSort(arr, 0, len(arr)-1)
